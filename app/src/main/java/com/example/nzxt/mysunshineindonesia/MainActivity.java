373417482e416f3b65f9f13e26ba41376f59ada1
sagnol10 <ari.setiaji3@gmail.com>
2017-11-04 13:34:06 +0700

    Binding view Layout to Java class

--- app/src/main/java/com/example/nzxt/mysunshineindonesia/MainActivity.java
+++ app/src/main/java/com/example/nzxt/mysunshineindonesia/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.nzxt.mysunshineindonesia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView weatherDayTitle;
+    ImageView imgWeather;
+    TextView weatherDesc;
+    TextView weatherTemperature;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weatherDayTitle = (TextView) findViewById(R.id.weather_day_title);
+        imgWeather = (ImageView) findViewById(R.id.img_weather);
+        weatherDesc = (TextView) findViewById(R.id.weather_desc);
+        weatherTemperature = (TextView) findViewById(R.id.weather_temperature);
+
+        weatherDayTitle.setText("Hari Minggu");
+        weatherDesc.setText("Hari Terang Benderang");
+        weatherTemperature.setText("30 Derajat aja");
+
+        imgWeather.setImageResource(R.drawable.weather_img);
     }
 }
